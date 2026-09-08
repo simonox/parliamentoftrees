295cc53425d20796b682d008d3c0aa6d6937e905
Oliver Ochs <oliver.ochs@information-architects.de>
2026-09-08 18:45:06 +0200

    Using the default OSM

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -41,7 +41,7 @@ export default function Map() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
         {locations.map((location, index) => (
           <Marker
